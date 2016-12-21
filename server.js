27a0e62ff7e09cb3616187c64c6e512c7d07bdd7
Chad Avalon <chad.avalon1@gmail.com>
2016-12-20 20:53:34 -0800

    run server test

--- server.js
+++ server.js
@@ -6,8 +6,9 @@ app.use(express.static('public'));
 const server =  app.listen(process.env.PORT || 8080);
 
 
+
 if (require.main === module) {
-  runServer().catch(err => console.error(err));
+  server().catch(err => console.error(err));
 };
 
 
