c712942b3f2fccef1876c5904d1df8d27a2f0eec
cavaloni <chad.avalon1@gmail.com>
2017-01-17 16:44:03 -0800

    fix directory structure update

--- trace.config.js
+++
@@ -1,5 +0,0 @@
-// trace.config.js
-module.exports = {
-  serviceName: 'Transcriptor',
-  apiKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjU4NjU1OGJmYmRjZGMzMDAwMWUyNzFlYiIsImlhdCI6MTQ4MzAzNjg2M30.XNd87HRsb6CGex9h-WR6cMBF6hKP3ZRRUB9XrtcY0TY'
-};
